55be4666dd16c7426235c41e3dde1a624a550ef5
LuckyHariharan <99484575+LuckyHariharan@users.noreply.github.com>
2024-01-11 19:39:06 -0500

    clean up code

--- src/popup/popup.tsx
+++ src/popup/popup.tsx
@@ -1,16 +1,73 @@
 import React, { useEffect, useState } from "react";
 import "./popup.css";
 import { useTransition, animated } from "react-spring";
-import ActuarialCalculation from "../calculations/ActuarialCalculation";
+import * as XLSX from "xlsx";
 import { SensitivityCalculation } from "../calculations/SensitivityCalculation";
 import handleFileUpload from "../excel/FileHandlers";
-import * as XLSX from "xlsx";
+import ActuarialCalculation from "../calculations/ActuarialCalculation";
 
 const Popup = () => {
+  // Add stages
   const Stages = {
-    Input: 1,
-    Result: 2,
+    Input: "input",
+    Result: "result",
   };
+  // State
+  const [isActuarialMode, setIsActuarialMode] = useState(true);
+  const [gender, setGender] = useState("");
+  const [smoking, setSmoking] = useState("");
+  const [periods, setPeriods] = useState("0");
+  const [periodsDisplay, setPeriodsDisplay] = useState("0");
+  const [interestRate, setInterestRate] = useState("%");
+  const [interestRateDisplay, setInterestRateDisplay] = useState("");
+  const [numericInterest, setNumericInterest] = useState(0);
+  const [paymentAmount, setPaymentAmount] = useState("$");
+  const [paymentFrequency, setPaymentFrequency] = useState("monthly");
+  const [paymentStartYear, setPaymentStartYear] = useState("0");
+  const [stage, setStage] = useState(Stages.Input);
+  const [result, setResult] = useState<number | null>(null);
+  const [ageDisplay, setAgeDisplay] = useState<number | "">(15);
+  const [age, setAge] = useState<number>(15);
+  const [ageError, setAgeError] = useState(false);
+  const [payPeriodError, setPayPeriodError] = useState(false);
+  const [smokingStatusError, setSmokingStatusError] = useState(false);
+  const [genderStatusError, setGenderStatusError] = useState(false);
+  const [formattedResult, setFormattedResult] = useState("");
+  const [disableButton, setDisableButton] = useState(true);
+  const [previousAge, setPreviousAge] = useState(15);
+  const [sensitivityResult, setSensitivityResult] =
+    useState<SensitivityResult | null>(null);
+
+  // SensitivityResult type definition
+  type SensitivityResult = {
+    lowerRangeAPV: number;
+    lowerCashValue65: number;
+    lowerCashValue85: number;
+    higherRangeAPV: number;
+    higherCashValue65: number;
+    higherCashValue85: number;
+    cashValue65: number;
+  };
+
+  // Function to format the interest rate input
+  const reformatInterestRateInput = (value: string): string => {
+    // Remove any non-digit characters (except ".")
+    value = value.replace(/[^\d.]/g, "");
+
+    // Ensure only one decimal point
+    const parts = value.split(".");
+    if (parts.length > 2) {
+      value = parts.slice(0, 2).join(".") + parts.slice(2).join("");
+    }
+
+    return value;
+  };
+
+  // Function to parse currency string
+  function parseCurrencyString(currencyString: string): number {
+    const sanitizedString = currencyString.replace(/[$,]/g, "");
+    return parseFloat(sanitizedString);
+  }
   const exportToXLSX = () => {
     // Create a new workbook
     const wb = XLSX.utils.book_new();
@@ -30,89 +87,23 @@ const Popup = () => {
     // Save the workbook as an XLSX file
     XLSX.writeFile(wb, "actuarial_data.xlsx");
   };
-
-  // State for Retirement or Actuarial Button
-  const [isActuarialMode, setIsActuarialMode] = useState<boolean>(true);
-
-  // State for dropdown selections
-  const [gender, setGender] = useState<string>("");
-  const [smoking, setSmoking] = useState<string>("");
-  const [periods, setPeriods] = useState<string>("0");
-  const [periodsDisplay, setPeriodsDisplay] = useState<string>("0");
-  const [interestRate, setInterestRate] = useState<string>("%");
-  const [interestRateDisplay, setInterestRateDisplay] = useState<string>("");
-  const [numericInterest, setNumericInterest] = useState<number>(0);
-  const [paymentAmount, setPaymentAmount] = useState<string>("$");
-  const [paymentFrequency, setPaymentFrequency] = useState<string>("monthly");
-  const [paymentStartYear, setPaymentStartYear] = useState<string>("0");
-  const [stage, setStage] = useState<number>(Stages.Input);
-  const [result, setResult] = useState<number | null>(null); // Result state
-  const [ageDisplay, setAgeDisplay] = useState<number | "">(15);
-  const [age, setAge] = useState<number>(15);
-  const [ageError, setAgeError] = useState<boolean>(false);
-  const [payPeriodError, setPayPeriodError] = useState<boolean>(false);
-  const [smokingStatusError, setSmokingStatusError] = useState<boolean>(false);
-  const [genderStatusError, setGenderStatusError] = useState<boolean>(false);
-  const [formattedResult, setFormattedResult] = useState<string>("");
-  const [disableButton, setDisableButton] = useState<boolean>(true);
-  const [previousAge, setPreviousAge] = useState(15); // Store the previous age
-  const [sensitivityResult, setSensitivityResult] =
-    useState<SensitivityResult>(null); // Initialize as null
-  // Define the type for your sensitivity result
-  type SensitivityResult = {
-    lowerRangeAPV: number;
-    lowerCashValue65: number;
-    lowerCashValue85: number;
-    higherRangeAPV: number;
-    higherCashValue65: number;
-    higherCashValue85: number;
-    cashValue65: number;
-  };
-  // Function to format the interest rate input
-  const reformatInterestRateInput = (value: string): string => {
-    // Remove any non-digit characters (except ".")
-    value = value.replace(/[^\d.]/g, "");
-
-    // Ensure only one decimal point
-    const parts = value.split(".");
-    if (parts.length > 2) {
-      value = parts.slice(0, 2).join(".") + parts.slice(2).join("");
-    }
-
-    return value;
-  };
-
-  function parseCurrencyString(currencyString: string): number {
-    // Remove special characters ('$' and ',') from the input string
-    const sanitizedString = currencyString.replace(/[$,]/g, "");
-
-    // Parse the sanitized string into a number
-    const numberValue = parseFloat(sanitizedString);
-
-    return numberValue;
-  }
+  // Function to format result
   const formatResult = (value: number): string => {
-    // Check if the value is not a number or is NaN
     if (isNaN(value) || typeof value !== "number") {
       return "$0.00";
     }
 
-    // Convert the number to a string and round it to two decimal places
     const formattedValue = value.toFixed(2);
-
-    // Split the formatted string into dollars and cents parts
     const [dollars, cents] = formattedValue.split(".");
-
-    // Add commas for thousands in the dollars part
     const formattedDollars = dollars.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 
-    // Reconstruct the formatted value with dollars and cents
     return `$${formattedDollars}.${cents}`;
   };
+
   useEffect(() => {
     if (!ageError && !payPeriodError) {
       const result = ActuarialCalculation(
-        numericInterest / 100, // Use the parsed interest rate
+        numericInterest / 100,
         parseInt(periods),
         gender,
         parseCurrencyString(paymentAmount),
@@ -120,7 +111,7 @@ const Popup = () => {
         age
       );
       const sensitivityResult = SensitivityCalculation(
-        numericInterest / 100, // Use the parsed interest rate
+        numericInterest / 100,
         parseInt(periods),
         gender,
         parseCurrencyString(paymentAmount),
@@ -142,24 +133,32 @@ const Popup = () => {
       setSmokingStatusError(false);
       setDisableButton(false);
     }
-  });
+  }, [
+    ageError,
+    payPeriodError,
+    gender,
+    age,
+    smoking,
+    numericInterest,
+    periods,
+    paymentAmount,
+  ]);
+
   // Function to handle the calculation when the button is clicked
   const handleCalculate = () => {
-    if (smoking == "") {
+    if (smoking === "") {
       setSmokingStatusError(true);
       return;
     }
-    if (gender == "") {
+    if (gender === "") {
       setGenderStatusError(true);
       return;
     }
 
-    // Check for age < 15
     if (age < 15 || age > 100) {
       setAgeError(true);
       return;
     } else {
-      // Check for payPeriod + age <= 100
       if (parseInt(periods) + age > 100) {
         setPayPeriodError(true);
         return;
@@ -177,7 +176,7 @@ const Popup = () => {
       setNumericInterest(parseFloat(interestRate.replace(/%/g, "")));
 
       const result = ActuarialCalculation(
-        numericInterest / 100, // Use the parsed interest rate
+        numericInterest / 100,
         parseInt(periods),
         gender,
         parseCurrencyString(paymentAmount),
@@ -197,7 +196,7 @@ const Popup = () => {
   };
 
   const handleBackToStage1 = () => {
-    setStage(Stages.Input); // Move back to the input stage
+    setStage(Stages.Input);
     setInputFieldsFocused({
       gender: false,
       age: false,
@@ -208,37 +207,27 @@ const Popup = () => {
       name: false,
     });
   };
+
   // Function to format the interest rate input
   const formatInterestRateInput = (value: string): string => {
-    // Remove any existing percent symbols from the value
     value = value.replace(/%/g, "");
-
-    // Add a percent symbol only if the last character is not already a percent symbol
     if (value[value.length - 1] !== "%") {
       value += "%";
     }
-
     return value;
   };
 
   // Function to format the payment amount input
   const formatPaymentAmountInput = (value: string): string => {
-    // Remove any existing dollar signs and commas from the value
     value = value.replace(/[$,]/g, "");
-
-    // Split the value into dollars and cents (if present)
     const [dollars, cents] = value.split(".");
-
-    // Add commas for thousands in the dollars part
     const formattedDollars = dollars.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 
-    // Reconstruct the formatted value with dollars and cents
-    if (cents !== undefined) {
-      return "$" + formattedDollars + "." + cents;
-    } else {
-      return "$" + formattedDollars;
-    }
+    return cents !== undefined
+      ? `$${formattedDollars}.${cents}`
+      : `$${formattedDollars}`;
   };
+
   // State to track if input fields have been focused (changed) by the user
   const [inputFieldsFocused, setInputFieldsFocused] = useState({
     gender: false,
@@ -249,7 +238,12 @@ const Popup = () => {
     paymentAmount: false,
     name: false,
   });
-  const handleInputBlur = (fieldName) => {
+
+  // Function to handle input blur
+  const handleInputBlur = (
+    event: React.FocusEvent<HTMLInputElement>,
+    fieldName: string
+  ) => {
     setInputFieldsFocused((prevInputFieldsFocused) => ({
       ...prevInputFieldsFocused,
       [fieldName]: true,
@@ -278,7 +272,7 @@ const Popup = () => {
             <option value="female">Female</option>
           </select>
           <input
-            className={`border rounded px-3  py-2 w-full ${
+            className={`border rounded px-3 py-2 w-full ${
               inputFieldsFocused.age ? "border-black" : ""
             }`}
             value={ageDisplay}
@@ -289,7 +283,7 @@ const Popup = () => {
               setAgeDisplay(parseInt(e.target.value));
               setAge(parseInt(e.target.value));
             }}
-            onBlur={handleInputBlur}
+            onBlur={(e) => handleInputBlur(e, "age")} // Pass the event and field name
             onClick={() => setAgeDisplay("")}
             min={15}
             max={100}
